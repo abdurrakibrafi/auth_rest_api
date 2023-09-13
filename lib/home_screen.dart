f95ca2e98c5f4e46b6d8ece4e76edc1fff2e94c4
abdurrakibrafi <aboudurrakib469@gmail.comm>
2023-09-13 13:38:30 +0600

    uploaded images in drawer using rest api

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key, required this.userEmail}) : super(key: key);
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
 
-  final String userEmail;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('user email : $userEmail'),
+        child: Text('Welcome'),
       ),
     );
   }
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,28 +1,133 @@
 import 'dart:convert';
-
+import 'dart:io';
 import 'package:auth_rest_api/home_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class LogInScreen extends StatefulWidget {
-  const LogInScreen({
-    Key? key,
-  }) : super(key: key);
+class UploadImageScreen extends StatefulWidget {
+  const UploadImageScreen({Key? key}) : super(key: key);
 
   @override
-  State<LogInScreen> createState() => _LogInScreenState();
+  _UploadImageScreenState createState() => _UploadImageScreenState();
 }
 
-class _LogInScreenState extends State<LogInScreen> {
-  //fort obsuecureObject show or hidden
-  bool obsecureObject = true;
+class _UploadImageScreenState extends State<UploadImageScreen> {
+  File? image;
+  final _picker = ImagePicker();
+  bool showSpinner = false;
 
-  var emailController = TextEditingController();
-  var passController = TextEditingController();
-  String userEmail = '';
+  Future getImage() async {
+    final pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
+
+    if (pickedFile != null) {
+      image = File(pickedFile.path);
+      setState(() {});
+    } else {
+      print('no image selected');
+    }
+  }
+
+  Future<void> uploadImage() async {
+    setState(() {
+      showSpinner = true;
+    });
+
+    var stream = new http.ByteStream(image!.openRead());
+    stream.cast();
+
+    var length = await image!.length();
+
+    var uri = Uri.parse('https://fakestoreapi.com/products');
+
+    var request = new http.MultipartRequest('POST', uri);
+
+    request.fields['title'] = "Static title";
+
+    var multiport = new http.MultipartFile('image', stream, length);
+
+    request.files.add(multiport);
+
+    var response = await request.send();
+
+    print(response.stream.toString());
+    if (response.statusCode == 200) {
+      setState(() {
+        showSpinner = false;
+      });
+      print('image uploaded');
+    } else {
+      print('failed');
+      setState(() {
+        showSpinner = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Upload Image'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: GestureDetector(
+                    onTap: () {
+                      uploadImage();
+                    },
+                    child: Text("Upload Image")),
+                accountEmail: Text("abhishekm977@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.orange,
+                  child: GestureDetector(
+                    onTap: () {
+                      getImage();
+                    },
+                    child: image == null
+                        ? Text('Pick Image')
+                        : Image.file(
+                            File(image!.path).absolute,
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.fill,
+                          ),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Settings"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.contacts),
+                title: Text("Contact Us"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
